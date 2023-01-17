8c89fdab5bccf727c8c73cad3fe9707ce2e7b0be
MarianellaGL <Marianella.gomezluna@gmail.com>
2023-01-16 21:32:00 -0300

    adding cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const app = express();
 const routes = require('./routes/routes');
 
+
 const PORT = 8080;
 
+app.use(cors())
 app.use('/api', routes);
 
 const server = app.listen(PORT, () => console.log(`Server started on port ${PORT}...`))
